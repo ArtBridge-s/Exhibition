da18d188eb4a4b8db960f14a71529114bdafbff7
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-14 17:53:43 +0900

    refactor: DTO 클래스에서 롬복 사용

--- src/main/java/com/artbridge/exhibition/service/dto/ExhibitionDTO.java
+++ src/main/java/com/artbridge/exhibition/service/dto/ExhibitionDTO.java
@@ -2,149 +2,23 @@ package com.artbridge.exhibition.service.dto;
 
 import com.artbridge.exhibition.domain.enumeration.Status;
 import java.io.Serializable;
-import java.util.Objects;
+import lombok.Data;
 
 /**
  * A DTO for the {@link com.artbridge.exhibition.domain.Exhibition} entity.
  */
 @SuppressWarnings("common-java:DuplicatedBlocks")
+@Data
 public class ExhibitionDTO implements Serializable {
 
     private String id;
-
     private String title;
-
     private String location;
-
     private String fee;
-
     private String contact;
-
     private String imgUrl;
-
     private String voPeriod;
-
     private String voArtist;
-
     private String voMember;
-
     private Status status;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getFee() {
-        return fee;
-    }
-
-    public void setFee(String fee) {
-        this.fee = fee;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public String getVoPeriod() {
-        return voPeriod;
-    }
-
-    public void setVoPeriod(String voPeriod) {
-        this.voPeriod = voPeriod;
-    }
-
-    public String getVoArtist() {
-        return voArtist;
-    }
-
-    public void setVoArtist(String voArtist) {
-        this.voArtist = voArtist;
-    }
-
-    public String getVoMember() {
-        return voMember;
-    }
-
-    public void setVoMember(String voMember) {
-        this.voMember = voMember;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ExhibitionDTO)) {
-            return false;
-        }
-
-        ExhibitionDTO exhibitionDTO = (ExhibitionDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, exhibitionDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "ExhibitionDTO{" +
-            "id='" + getId() + "'" +
-            ", title='" + getTitle() + "'" +
-            ", location='" + getLocation() + "'" +
-            ", fee='" + getFee() + "'" +
-            ", contact='" + getContact() + "'" +
-            ", imgUrl='" + getImgUrl() + "'" +
-            ", voPeriod='" + getVoPeriod() + "'" +
-            ", voArtist='" + getVoArtist() + "'" +
-            ", voMember='" + getVoMember() + "'" +
-            ", status='" + getStatus() + "'" +
-            "}";
-    }
 }
--- src/main/java/com/artbridge/exhibition/service/dto/LikeDTO.java
+++ src/main/java/com/artbridge/exhibition/service/dto/LikeDTO.java
@@ -1,72 +1,16 @@
 package com.artbridge.exhibition.service.dto;
 
 import java.io.Serializable;
-import java.util.Objects;
+import lombok.Data;
 
 /**
  * A DTO for the {@link com.artbridge.exhibition.domain.Like} entity.
  */
 @SuppressWarnings("common-java:DuplicatedBlocks")
+@Data
 public class LikeDTO implements Serializable {
 
     private String id;
-
     private Long voMember;
-
     private ExhibitionDTO artwork;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Long getVoMember() {
-        return voMember;
-    }
-
-    public void setVoMember(Long voMember) {
-        this.voMember = voMember;
-    }
-
-    public ExhibitionDTO getArtwork() {
-        return artwork;
-    }
-
-    public void setArtwork(ExhibitionDTO artwork) {
-        this.artwork = artwork;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof LikeDTO)) {
-            return false;
-        }
-
-        LikeDTO likeDTO = (LikeDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, likeDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "LikeDTO{" +
-            "id='" + getId() + "'" +
-            ", voMember=" + getVoMember() +
-            ", artwork=" + getArtwork() +
-            "}";
-    }
 }
