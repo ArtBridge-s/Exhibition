96b5e8af41f24299320eca180082bbdf262810e2
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-17 07:27:40 +0900

    feat(CCSZ-24): 관리자의 전시회 정보 추가 비즈니스 로직, 파사드 구현

--- src/main/java/com/artbridge/exhibition/application/service/ExhibitionService.java
+++ src/main/java/com/artbridge/exhibition/application/service/ExhibitionService.java
@@ -63,4 +63,6 @@ public interface ExhibitionService {
     void authorizeDelete(String id);
 
     void deleteByAdmin(String id);
+
+    ExhibitionDTO saveByAdmin(MultipartFile file, ExhibitionDTO exhibitionDTO);
 }
