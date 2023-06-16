8e17869de2d6c6e1564df6fd07d2c60551a7f783
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-17 07:55:11 +0900

    refactor: 도메인 서비스 예외 처리 커스텀

--- src/main/java/com/artbridge/exhibition/domain/exception/CurrentUserIdNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.artbridge.exhibition.domain.exception;
-
-public class CurrentUserIdNotAvailableException extends RuntimeException {
-    public CurrentUserIdNotAvailableException() {
-        super("Current user ID is not available.");
-    }
-}
