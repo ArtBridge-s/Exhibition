3e8c3e13639fea501e14f0c8145e6a7734fd8c18
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-17 01:02:25 +0900

    ref(CCSZ-9): Response 추가

---
+++ src/main/java/com/artbridge/exhibition/web/response/Exhibition_GET_LIST_STATUS_OK_Res.java
@@ -0,0 +1,21 @@
+package com.artbridge.exhibition.web.response;
+
+import com.artbridge.exhibition.application.dto.ArtistDTO;
+import com.artbridge.exhibition.application.dto.MemberDTO;
+import com.artbridge.exhibition.application.dto.PeriodDTO;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Exhibition_GET_LIST_STATUS_OK_Res implements Serializable {
+    private String id;
+    private String title;
+    private String location;
+    private String fee;
+    private String contact;
+    private String imgUrl;
+    private PeriodDTO periodDTO;
+    private ArtistDTO artistDTO;
+    private MemberDTO createdMemberDTO;
+}
