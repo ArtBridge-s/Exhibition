a2858fd5ed1fa409b3f4df897bfcfed6f6c2bc16
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-17 06:03:53 +0900

    feat(CCSZ-14): 전시회 수정 요청 상태 변경 프레젠테이션 구현

--- src/main/java/com/artbridge/exhibition/web/rest/ExhibitionResource.java
+++ src/main/java/com/artbridge/exhibition/web/rest/ExhibitionResource.java
@@ -217,6 +217,15 @@ public class ExhibitionResource {
     }
 
 
+    @PatchMapping("/exhibitions/request/ok/{id}")
+    public ResponseEntity<ExhibitionDTO> requestOk(@PathVariable(value = "id") final String id) {
+        log.debug("REST request to update Exhibition : {}", id);
+
+        Optional<ExhibitionDTO> result = exhibitionService.requestOk(id);
+        return ResponseUtil.wrapOrNotFound(result, HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, id));
+    }
+
+
     /**
      * {@code GET  /exhibitions/:id} : get the "id" exhibition.
      *
