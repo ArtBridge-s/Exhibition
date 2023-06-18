2eae9309d283cd82652b1e6e6c5d8d584997bed7
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-18 16:27:02 +0900

    hotfix: Exhibition 에 String content 추가

--- src/main/java/com/artbridge/exhibition/web/request/Exhibition_POST_Req.java
+++ src/main/java/com/artbridge/exhibition/web/request/Exhibition_POST_Req.java
@@ -2,14 +2,15 @@ package com.artbridge.exhibition.web.request;
 
 import com.artbridge.exhibition.application.dto.ArtistDTO;
 import com.artbridge.exhibition.application.dto.PeriodDTO;
-import lombok.Data;
-
 import java.io.Serializable;
+import lombok.Data;
 
 @Data
 public class Exhibition_POST_Req implements Serializable {
+
     private String title;
     private String location;
+    private String content;
     private String fee;
     private String contact;
     private PeriodDTO periodDTO;
